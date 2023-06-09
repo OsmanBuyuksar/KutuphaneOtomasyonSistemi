c47f87af667f175a1a073b2311eb55b016525357
OsmanBuyuksar <osmanbuyuksar@gmail.com>
2023-06-09 16:40:13 +0300

    Changes to the UserWindow class

--- index.py
+++ index.py
@@ -7,28 +7,16 @@ import icons_rc
 from PyQt5.uic import loadUiType
 
 ui, base = loadUiType('UserWindow.ui')
-print(ui)
-print(base)
 
 class UserWindow(base, ui):
-    def __init__(self, ui):
-        self.base = base()
-        self.ui = ui
-        self.ui.setupUi(self.base)
-
-    def show(self):
-        self.base.show()
+    def __init__(self):
+        base.__init__(self)
+        self.setupUi(self)
 
 def main():
     app = QApplication(sys.argv)
-    widget = ui()
-    window = base()
-    widget.setupUi(window)
-    window.show()
-
-    uWindow = UserWindow(ui())
+    uWindow = UserWindow()
     uWindow.show()
-    #window.base.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
